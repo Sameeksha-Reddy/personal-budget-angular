df46a37ed8ae8dc17c2ad0b3b76c7862870b395e
sameekksha <sameekshareddykv@gmail.com>
2023-09-17 16:32:22 -0400

    Loading data from JSON file

--- server.js
+++ server.js
@@ -1,32 +1,29 @@
 const express = require ('express') ;
 const app = express();
 const port = 3000;
+const fs = require('fs');
 
 app.use('/', express.static('public'));
-
-const budget = {
-    myBudget: [
-    {
-        title: 'Eat Out',
-        budget: 25
-    },
-    {
-        title: 'Rent',
-        budget: 375
-    },
-    {
-        title: 'Grocery',
-        budget: 110
-    },
-  ]
-};
 
 app.get('/hello', (req, res) => {
     res.send('Hello World!');
 });
 
 app.get('/budget', (req, res) => {
-    res.json(budget);
+    fs.readFile('mybudget.json', 'utf8', (err, data) => {
+        if (err) {
+            console.error("Error reading JSON file: " + err);
+            res.status(500).json({ error: "Internal Server Error" });
+        } else {
+            try {
+                const mybudgetData = JSON.parse(data);
+                res.json(mybudgetData);
+            } catch (parseErr) {
+                console.error("Error parsing JSON data: " + parseErr);
+                res.status(500).json({ error: "Internal Server Error" });
+            }
+        }
+    });
 });
 
 app.listen(port, () => {
