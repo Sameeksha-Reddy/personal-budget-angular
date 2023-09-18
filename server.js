d330fb6baa7511c9424a2281240b735936fe9828
sameekksha <sameekshareddykv@gmail.com>
2023-09-17 23:42:39 -0400

    Added D3JS chart

--- server.js
+++ server.js
@@ -1,31 +1,19 @@
-const express = require ('express') ;
+
+const express = require('express');
+const fs = require("fs");
 const app = express();
 const port = 3000;
-const fs = require('fs');
 
 app.use('/', express.static('public'));
 
 app.get('/hello', (req, res) => {
-    res.send('Hello World!');
+  res.send('Hello World!');
 });
 
 app.get('/budget', (req, res) => {
-    fs.readFile('mybudget.json', 'utf8', (err, data) => {
-        if (err) {
-            console.error("Error reading JSON file: " + err);
-            res.status(500).json({ error: "Internal Server Error" });
-        } else {
-            try {
-                const mybudgetData = JSON.parse(data);
-                res.json(mybudgetData);
-            } catch (parseErr) {
-                console.error("Error parsing JSON data: " + parseErr);
-                res.status(500).json({ error: "Internal Server Error" });
-            }
-        }
-    });
-});
+  res.sendFile("/Users/sameekksha/Desktop/NBAD/Week3/personal-budget1/server.json");
+})
 
 app.listen(port, () => {
-    console.log('Example app listening at http://localhost:${port}');
+  console.log(`Example app listening at http://localhost:${port}`)
 });
