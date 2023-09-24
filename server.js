07f99fdc86756617e8ba75d57b22ad29a396ee66
sameekksha <sameekshareddykv@gmail.com>
2023-09-24 18:16:55 -0400

    cors downloaded

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
+// Budget API
 
 const express = require('express');
+const cors = require('cors');
 const fs = require("fs");
 const app = express();
 const port = 3000;
 
-app.use('/', express.static('public'));
+app.use(cors());
 
 app.get('/hello', (req, res) => {
   res.send('Hello World!');
@@ -15,5 +17,5 @@ app.get('/budget', (req, res) => {
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+  console.log(`API served at at http://localhost:${port}`)
 });
