8ce75f018ab1455bd0c2f5f55ec36e7dd41a2a09
sameekksha <sameekshareddykv@gmail.com>
2023-09-24 22:37:49 -0400

    Cors

--- server.js
+++ server.js
@@ -1,21 +1,18 @@
-// Budget API
+//Budget API
 
 const express = require('express');
 const cors = require('cors');
-const fs = require("fs");
 const app = express();
 const port = 3000;
 
 app.use(cors());
 
-app.get('/hello', (req, res) => {
-  res.send('Hello World!');
-});
 
 app.get('/budget', (req, res) => {
-  res.sendFile("/Users/sameekksha/Desktop/NBAD/Week3/personal-budget1/server.json");
-})
+  const samp=require('./pb.json')
+  res.json(samp);
+});
 
 app.listen(port, () => {
-  console.log(`API served at at http://localhost:${port}`)
+    console.log(`Example API served at http://localhost:${port}!`);
 });
